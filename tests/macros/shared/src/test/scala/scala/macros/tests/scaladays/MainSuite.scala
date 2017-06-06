eb369a73ed9fa44764ecb438659d61a26f3a16e1
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-06-06 13:30:05 +0200

    Enable macro annotation test.

--- tests/macros/shared/src/test/scala/scala/macros/tests/scaladays/MainSuite.scala
+++ tests/macros/shared/src/test/scala/scala/macros/tests/scaladays/MainSuite.scala
@@ -1,26 +1,24 @@
 package scala.macros.tests
 package scaladays
 
+import java.io.ByteArrayOutputStream
 import org.junit._
 import org.junit.runner._
 import org.junit.runners._
 import org.junit.Assert._
-import scala.macros.config._
 
 @RunWith(classOf[JUnit4])
 class MainSuite {
   @Test
   def simple: Unit = {
-    // TODO: Scalac produces an error if we try to create a main method in a local object.
-    // No idea why it does that, but that actually prevents us from testing the @main annotation.
-    //
-    // var printed = false
-    // def println(s: String): Unit = { printed = true }
-    // @main
-    // object Test {
-    //   println("hello world")
-    // }
-    // Test.main(Array[String]())
-    // assertEquals(true, printed)
+    val out = new ByteArrayOutputStream()
+    Console.withOut(out) {
+      Test.main(Array[String]())
+    }
+    assertEquals(out.toString, "hello world\n")
   }
+}
+@main
+object Test {
+  println("hello world")
 }
